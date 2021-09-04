a96dd758af5bcc9c978e3994dbb8de4fdfbb58d1
acidjuice <acidjuice123@gmail.com>
2021-09-05 01:07:55 +0300

    bootstrap+head, base.html added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    url = models.URLField(blank=True)
+    date = models.DateField()
